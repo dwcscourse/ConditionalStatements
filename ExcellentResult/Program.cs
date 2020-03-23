bd87cadcc9191585ba06f621df8c6195f874aa65
Tsvetomira Dimitrova <tsvetomira.dimitrova@docuware.com>
2020-03-23 11:57:34 +0200

    add new exercises

---
+++ ExcellentResult/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ExcellentResult
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double result = double.Parse(Console.ReadLine());
+            if (result >= 5.5)
+            {
+                Console.WriteLine("Excellent!");
+            }
+        }
+    }
+}
---
+++ Number100-200/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Number100_200
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
