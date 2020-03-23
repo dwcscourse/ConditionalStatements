9246c76226801852d6f447d6250ee0e6d8e1ca94
Tsvetomira Dimitrova <tsvetomira.dimitrova@docuware.com>
2020-03-23 12:19:19 +0200

    Add more exercises

---
+++ AreaOfFigures/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace AreaOfFigures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //(square, rectangle, circle или triangle).
+            string figure = Console.ReadLine();
+            double area = 0;
+
+            if (figure == "square")
+            {
+                double num = double.Parse(Console.ReadLine());
+                area = num * num;
+            }
+            else if (figure =="rectangle")
+            {
+                double sideA = double.Parse(Console.ReadLine());
+                double sideB = double.Parse(Console.ReadLine());
+                area = sideA * sideB;
+            }
+            else if (figure == "circle")
+            {
+                double radius = double.Parse(Console.ReadLine());
+                area = Math.PI * radius * radius;
+            }
+            else if (figure == "triangle")
+            {
+                double sideA = double.Parse(Console.ReadLine());
+                double heightA = double.Parse(Console.ReadLine());
+                area = sideA * heightA / 2;        
+            }
+
+            Console.WriteLine($"{area:f3}");
+        }
+    }
+}
--- Number100-200/Program.cs
+++ Number100-200/Program.cs
@@ -6,7 +6,20 @@ namespace Number100_200
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number < 100)
+            {
+                Console.WriteLine("Less than 100");
+            }
+            else if (number >= 100 && number <=200)
+            {
+                Console.WriteLine("Between 100 and 200");
+            }
+            else
+            {
+                Console.WriteLine("Greater than 200");
+            }
         }
     }
 }
